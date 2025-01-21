ba194dd7f17bccd1f6bd6f7d70451edce8e3b82f
mitreb <tmitreb@gmail.com>
2025-01-21 19:06:57 +0100

    Fetch movies on component mount

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
   <v-container>
     <p class="text-h4 mt-16 mb-6">Feature Today</p>
+
+    <div v-if="isLoading" class="text-center">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </div>
+
+    <v-row v-else>
+      <v-col v-for="movie in movies" :key="movie.id" cols="12" md="4">
+        <v-card class="mb-4">
+          <v-card-title>{{ movie.title }}</v-card-title>
+          <v-card-actions> </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { movies, isLoading, fetchMovies } = useMovies();
+
+onMounted(() => {
+  fetchMovies();
+});
+</script>
