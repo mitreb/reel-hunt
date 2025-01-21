4989768f33d0c9aaf6c38a8eb65f418f3bcf9de1
mitreb <tmitreb@gmail.com>
2025-01-21 10:32:35 +0100

    Add UnoCSS configuration and update dependencies in package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module'],
+  modules: ['vuetify-nuxt-module', '@unocss/nuxt'],
+  vuetify: {
+    moduleOptions: {},
+    vuetifyOptions: {
+      icons: {
+        defaultSet: 'mdi',
+        sets: ['mdi', 'fa'],
+      },
+    },
+  },
 });
