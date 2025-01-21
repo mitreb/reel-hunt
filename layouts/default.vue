b5c46d43191a3987e2dd06a3c63723ac84aab4b7
mitreb <tmitreb@gmail.com>
2025-01-21 10:34:44 +0100

    Add default layout with Vuetify components

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar extended>
+      <v-icon>mdi-home</v-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-input></v-input>
+    </v-app-bar>
+
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts"></script>
