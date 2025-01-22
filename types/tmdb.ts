e61acccf28ccd21c5aa50fe53b5d9d576f1fe69f
mitreb <tmitreb@gmail.com>
2025-01-22 14:50:20 +0100

    Enhance Movie interface with additional properties

--- types/tmdb.ts
+++ types/tmdb.ts
@@ -1,10 +1,18 @@
 export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
   id: number;
-  title: string;
+  original_language: string;
+  original_title: string;
   overview: string;
-  poster_path: string | null;
+  popularity: number;
+  poster_path: string;
   release_date: string;
+  title: string;
+  video: boolean;
   vote_average: number;
+  vote_count: number;
 }
 
 export interface TMDBResponse {
