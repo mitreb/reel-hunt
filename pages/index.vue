1a07a9de5bdec14cbac1b325683f41d97880a2ee
mitreb <tmitreb@gmail.com>
2025-01-23 11:27:31 +0100

    Use MovieCard component in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,52 +12,17 @@
         lg="3"
         xl="2"
       >
-        <v-card v-if="movie" class="bg-transparent">
-          <div class="position-relative">
-            <v-img
-              :src="
-                movie.poster_path ? `${imageBaseUrl}${movie.poster_path}` : ''
-              "
-              aspect-ratio="2/3"
-              cover
-            ></v-img>
-            <ScoreCircle :score="movie.vote_average" />
-          </div>
-          <v-card-text class="px-0">
-            <h3>{{ movie.title }}</h3>
-            <p class="text-grey text-subtitle-2">
-              {{ formatDateToMonthYear(movie.release_date) }}
-            </p>
-          </v-card-text>
-        </v-card>
+        <HomeMovieCard :movie="movie" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { formatDateToMonthYear } from '@/utils';
 const moviesStore = useMoviesStore();
 const { movies } = storeToRefs(moviesStore);
-const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 
 onMounted(() => {
   moviesStore.fetchMovies();
 });
 </script>
-
-<style scoped>
-.circle {
-  width: 3rem;
-  height: 3rem;
-  background-color: white;
-  border-radius: 50%;
-  position: absolute;
-  z-index: 1;
-  bottom: -0.5rem;
-  left: 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
