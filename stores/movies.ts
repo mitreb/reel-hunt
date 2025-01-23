5eab0044b5a67d6cc7304e8454dc965958ce9a82
mitreb <tmitreb@gmail.com>
2025-01-23 10:29:41 +0100

    Add 'movies' store

---
+++ stores/movies.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia';
+import type { Movie, TMDBResponse } from '~/types';
+
+export const useMoviesStore = defineStore('movies', () => {
+  const movies = ref<Movie[]>([]);
+  const isLoading = ref(false);
+
+  async function fetchMovies(page = 1) {
+    try {
+      isLoading.value = true;
+      const response = await $fetch<TMDBResponse>('/api/movies/discover', {
+        params: { page },
+      });
+      movies.value = response.results;
+    } catch (error) {
+      const messageStore = useMessageStore();
+      messageStore.message = (error as Error).message;
+      messageStore.showMessage = true;
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  return {
+    movies,
+    isLoading,
+    fetchMovies,
+  };
+});
