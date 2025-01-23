5359f14ea83b894e4482b643233f20e1f58658b6
mitreb <tmitreb@gmail.com>
2025-01-23 09:41:41 +0100

    Add Message component

---
+++ components/global/Message.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-dialog v-model="messageStore.showMessage" width="400">
+    <v-card>
+      <v-card-text>
+        <p>{{ messageStore.message }}</p>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+const messageStore = useMessageStore();
+</script>
