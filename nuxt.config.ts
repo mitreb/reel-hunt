80a0ae5a496d1c79fa006268af3c13ae21abfe8a
mitreb <tmitreb@gmail.com>
2025-01-23 09:20:53 +0100

    Install Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ const ReelHuntTheme = {
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module', '@unocss/nuxt'],
+  modules: ['vuetify-nuxt-module', '@unocss/nuxt', '@pinia/nuxt'],
   vuetify: {
     moduleOptions: {},
     vuetifyOptions: {
