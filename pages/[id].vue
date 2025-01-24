9772ca35c427bedc0d1d833e504ef514e0c1ce97
mitreb <tmitreb@gmail.com>
2025-01-24 12:34:46 +0100

    Add a movie page

---
+++ pages/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <h1 class="text-h4 mt-16 mb-6">
+      {{ route.params.id }}
+    </h1>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>
