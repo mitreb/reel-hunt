7c7e329c316182efc9b15d845bc9e7f80c767fe9
mitreb <tmitreb@gmail.com>
2025-01-24 15:58:21 +0100

    Fetch and display movie details on the movie page

--- pages/[id].vue
+++ pages/[id].vue
@@ -3,9 +3,18 @@
     <h1 class="text-h4 mt-16 mb-6">
       {{ route.params.id }}
     </h1>
+    <p v-if="movieDetails">{{ JSON.stringify(movieDetails) }}</p>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import type { MovieDetails } from '~/types';
 const route = useRoute();
+
+const movieDetails = ref<MovieDetails | null>(null);
+
+const { data } = await useFetch<MovieDetails>(`/api/movies/${route.params.id}`);
+movieDetails.value = data.value;
+
+console.log(movieDetails.value);
 </script>
